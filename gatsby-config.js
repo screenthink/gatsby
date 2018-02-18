27800a8ac02912c8e9643695570ca17c71d7e40b
Marlon Lulgjuraj <marlon@screenthink.com>
2018-02-18 13:39:07 -0500

    Add gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+// gatsby-config.js
+
+require('dotenv').config();
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID || '',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || ''
+      }
+    }
+  ]
+};
